1ccc46e802f4b4ac3d0eb26b4f82e8b122b89694
Lucas Val <lucas.val@dextra.sw.com>
2021-03-03 15:20:09 -0300

    render and config

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,17 +2,35 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
-	handler "github.com/lcsval/gohtml/pkg/handlers"
+	"github.com/lcsval/gohtml/pkg/config"
+	"github.com/lcsval/gohtml/pkg/handlers"
+	"github.com/lcsval/gohtml/pkg/render"
 )
 
 const portNumber = ":8080"
 
 // main is the main application function
 func main() {
-	http.HandleFunc("/", handler.Home)
-	http.HandleFunc("/about", handler.About)
+	var app config.AppConfig
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+	http.HandleFunc("/", handlers.Repo.Home)
+	http.HandleFunc("/about", handlers.Repo.About)
 
 	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
 	_ = http.ListenAndServe(portNumber, nil)
--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -3,15 +3,40 @@ package handlers
 import (
 	"net/http"
 
+	"github.com/lcsval/gohtml/pkg/config"
+	"github.com/lcsval/gohtml/pkg/models"
 	"github.com/lcsval/gohtml/pkg/render"
 )
 
+var Repo *Repository
+
+type Repository struct {
+	App *config.AppConfig
+}
+
+// NewRepo creates a new repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+// NewHandlers sets the repository for the handlers
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
 // Home is the home page handler
-func Home(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "home.page.tmpl")
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
 }
 
 // About is the about page handler
-func About(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "about.page.tmpl")
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
+	stringMap := make(map[string]string)
+	stringMap["test"] = "Hello, again"
+
+	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
+		StringMap: stringMap,
+	})
 }
